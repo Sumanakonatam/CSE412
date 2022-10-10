933ec1259574e40d9b3de43e5633f6a7c377790e
Sumanakonatam <skonatam@asu.edu>
2022-10-09 17:04:34 -0700

    implementing openstreet map

--- src/App.js
+++ src/App.js
@@ -2,27 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js'
 import Filters from './components/Filters.js'
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+
 
 function App() {
+  const position = [51.505, -0.09]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Map></Map>
-        <Filters></Filters>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
