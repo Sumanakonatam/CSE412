f67070751325fcddd8328c6b52d9bf4b16c6fd15
Sameeh Gafoor Suhail <sameehsuhail5@gmail.com>
2022-11-27 13:05:18 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import React, { useEffect, useState } from "react";
+
 import './App.css';
-import Map, {FullscreenControl, GeolocateControl, Marker, NavigationControl} from "react-map-gl"
-import { Card,Text } from '@nextui-org/react';
-import { NextUIProvider } from '@nextui-org/react';
+import Mapp from './Map';
 
 function App() {
-  const[lat, setLat] = useState(47.6038321);
-  const[lng, setLng] = useState(-122.330062);
-
-
   return (
     <div className="App">
-      <NextUIProvider>
-
-      
-
-      <Map
-      mapboxAccessToken="pk.eyJ1Ijoic2FtZWVoc3VoYWlsIiwiYSI6ImNsYXltY3YwMjEwZWwzbm44NWNrZDFuNHIifQ.TxMGHqwzrfCJ81W-H0xI5w"
-      style={{
-        width:"80vw",
-        height:"100vh",
-        position:"relative",
-        paddingLeft:"20vw"
-      }}
-
-      
-      
-      mapStyle="mapbox://styles/sameehsuhail/claymls6e004115mxig2eme72"
-      >
-        
-        <NavigationControl
-        position="top-right"
-        />
-        
-        <FullscreenControl/>
-        <GeolocateControl/>
-        </Map>
-        </NextUIProvider>
+      <Mapp />
     </div>
   );
 }
 
 export default App;
-/**
-      <Card isHoverable variant="bordered" css={{ 
-        mw: "400px",
-        width:"20vw",
-        height:"100vh",
-        position:"absolute",
-     }}>
-      <Card.Body>
-        <Text>Seattle</Text>
-      </Card.Body>
-    </Card>
-      
-      <Marker
-        longitude={lng}
-        latitude={lat}
-        />*/
