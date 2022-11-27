a2378deab3aaab70da88acfb417f2e77e373d7c6
Sameeh Gafoor Suhail <sameehsuhail5@gmail.com>
2022-11-27 13:10:14 -0800

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -6,22 +6,22 @@ mapboxgl.accessToken='pk.eyJ1Ijoic2FtZWVoc3VoYWlsIiwiYSI6ImNsYXltY3YwMjEwZWwzbm4
 
 const data = [
 	{
-		"location": "Manhattan Ave & Norman Ave at NE corner",
-		"city": "Brooklyn",
-		"state": "New York",
-		"coordinates": [-73.9516030004786,40.72557300071668],
+		"location": "West Seattle",
+		"city": "Seattle",
+		"state": "Washington",
+		"coordinates": [-122.3868,47.5667],
 	},
 	{
-		"location": "6th Ave & 42nd St at NW corner",
-		"city": "Manhattan",
-		"state": "New York",
-		"coordinates": [-73.98393399979334,40.75533300052329],
+		"location": "Central District",
+		"city": "Seattle",
+		"state": "Washington",
+		"coordinates": [-122.2964,47.6088],
 	},
 	{
-		"location": "Essex St & Delancey St at SE corner",
-		"city": "Manhattan",
-		"state": "New York",
-		"coordinates": [-73.9882730001973,40.718207001246554],
+		"location": "Capitol hill",
+		"city": "Seattle",
+		"state": "Washington",
+		"coordinates": [-122.3222,47.6253],
 	}
 ]
 
